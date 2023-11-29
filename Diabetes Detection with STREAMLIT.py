d518f9df04f467c21332d4fc5d31e78f25be811b
bharath2468 <bharath29704@gmail.com>
2023-11-29 21:25:07 +0530

    Model saved using Pickle

--- Diabetes Detection with STREAMLIT.py
+++ Diabetes Detection with STREAMLIT.py
@@ -3,6 +3,7 @@ from PIL import Image
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
 
 df=pd.read_csv("C:/Users/Bharath/Downloads/diabetes.csv")
 
@@ -25,11 +26,23 @@ y=df.iloc[:,-1]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
 
+#Standardization
+from sklearn.preprocessing import StandardScaler
+std=StandardScaler()
+X_train=std.fit_transform(X_train)
+X_test=std.transform(X_test)
+dummy='''
 # Model Building
 from sklearn.ensemble import RandomForestClassifier
 model=RandomForestClassifier()
 model.fit(X_train,y_train)
 y_pred=model.predict(X_test)
+
+#Saving the Model
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+'''
+
 def user():
     st.sidebar.title('Enter Input')
     preg=st.sidebar.slider('Pregnancies',25,0)
@@ -46,6 +59,12 @@ def user():
     user_input=pd.DataFrame(userinput,index=[0])
     return user_input
 
+#Loading Model
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+y_pred=model.predict(X_test)
+
+st.subheader('Output')
 user_input=user()
 user_output=model.predict(user_input)
 st.divider()
